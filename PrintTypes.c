44459d40a287f20ef1c7ea7eb35e3eb5a741ce04
Ken Lamarche <ken@valuedprojects.org>
2012-12-02 20:55:51 -0800

    Combine this program with another that was printing types sizes also.

--- PrintTypes.c
+++ PrintTypes.c
@@ -9,6 +9,13 @@ int main (int argc, char** argv)
 	unsigned long long ul1 = 0x1234567890123456ULL;
         char * str;
 
+        int a_int;
+        float b_float;
+        double c_double;
+        long d_long;
+        char e_array100[100];
+
+
 	// Print types and sizes:
 	printf("%s: Print data types and sizes\n", *argv);
 	printf("short s1 = 0x1234..... >%d< >0x%04X<\n", s1, s1);
@@ -16,6 +23,12 @@ int main (int argc, char** argv)
 	printf("long long l1 = 0x1234567890123456LL..... >%lld< >0x%16llX<\n", l1, l1);
 	printf("unsigned long long ul1 = 0x1234567890123456ULL..... >%llu< >0x%16llX<\n", ul1, ul1);
         printf("sizeof(char *): %ld\n", sizeof(char*));
+
+        printf("sizeof(a_int): %lu\n",sizeof(a_int));
+        printf("sizeof(b_float): %lu\n",sizeof(b_float));
+        printf("sizeof(c_double): %lu\n",sizeof(c_double));
+        printf("sizeof(d_long): %lu\n",sizeof(d_long));
+        printf("sizeof(e_array100): %lu\n",sizeof(e_array100));
 
 	return 0;
 }
